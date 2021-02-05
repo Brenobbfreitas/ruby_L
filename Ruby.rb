43557008d28082c3b8c62554e372ea093f839cf1
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-05 10:17:20 -0300

    Add files via upload
    new metodos

--- Ruby.rb
+++ Ruby.rb
@@ -1,3 +1,7 @@
-name = "Breno Barreto".reverse
+metodos -> acionados pelo .lenght -> quantidade de valores na string, .reverse, .upcase, .dowcase, .capitalize -> primeira letra maiuscula
+.include? "" -> responsavel por verificar a string com  o parametro escolhido
 
-puts reverse
+podemos intercalar os metodos
+
+You can combine boolean operators in your expressions. Combinations like
+(x && (y || w)) && z
