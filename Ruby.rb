9a0426028e548fd836a6fdbed6a83fcb207e0d6b
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-08 07:52:31 -0300

    Add files via upload
    new loops type

--- Ruby.rb
+++ Ruby.rb
@@ -1,7 +1,20 @@
 metodos -> acionados pelo .lenght -> quantidade de valores na string, .reverse, .upcase, .dowcase, .capitalize -> primeira letra maiuscula
 .include? "" -> responsavel por verificar a string com  o parametro escolhido
-
+gsub!(/string/, "string alteradora")
 podemos intercalar os metodos
 
 You can combine boolean operators in your expressions. Combinations like
 (x && (y || w)) && z
+
+loop operators while and for 
+
+diferença -> while continua o loop até a condição se tornar true 
+          -> for premedita o fim do loop 
+            
+Tipos de loop 
+i = 0
+loop do
+  i += 1
+  print "#{i}"
+  break if i > 5
+end
