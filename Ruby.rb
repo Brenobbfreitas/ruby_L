33eb43db743178904a3e70d0def77ae3bfd90580
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-21 21:01:03 -0300

    Add files via upload
    loops

--- Ruby.rb
+++ Ruby.rb
@@ -1,5 +1,4 @@
-metodos -> acionados pelo .lenght -> quantidade de valores na string, .reverse, .upcase, .dowcase, .capitalize -> primeira letra maiuscula
-.include? "" -> responsavel por verificar a string com  o parametro escolhido
+
 gsub!(/string/, "string alteradora")
 podemos intercalar os metodos
 
@@ -16,12 +15,55 @@ diferença -> while continua o loop até a condição se tornar true
           -> for premedita o fim do loop 
             
 Tipos de loop 
+{
 i = 0
 loop do
   i += 1
   print "#{i}"
   break if i > 5
 end
+}
+
+loop interator -> aplica uma expressão a cada elemento de um objeto; Pouco poderoso
+ {
+  array=[1,2,3,4,5]
+  
+  array.each do |x|
+    x += 10 
+    puts "#{x}"
+}
+
+loop while {
+  i = 0 
+  while i < 100 do (enquanto)
+    i += 1
+    puts i 
+  end
+}
+
+loop until{
+  j = 3
+until j == 0 do (Quando)
+  print j
+  j -= 1
+end
+}
+
+loop for{
+  i = 0 
+for i in 1..50
+  print i 
+end
+}
+
+loop break {
+  m = 0
+  loop do
+  m += 1
+  print m
+  break if m == 10 (condição para parar o loop)
+end
+}
 
 
 next if usado em um loop para passar por certos elementos e pular para a iteração
