c45861f1b55baaceee9a0123b35f800c56ddc5bc
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-21 21:02:13 -0300

    Add files via upload
    add new metodos

---
+++ metodos.rb
@@ -0,0 +1,11 @@
+metodos ->
+{
+Os metodos são acionados pelo "." 
+.lenght -> quantidade de valores na string, 
+.reverse, 
+.upcase, 
+.dowcase, 
+.capitalize -> primeira letra maiuscula
+.include? "" -> responsavel por verificar a string com  o parametro escolhido
+10.times{print "asdhauhda"} -> um for campacto
+}
