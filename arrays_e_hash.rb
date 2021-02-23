1e4c900c11adc95e116fb7633e2ae33527783d10
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-23 18:21:41 -0300

    Add files via upload
    arrays e hashes

---
+++ arrays_e_hash.rb
@@ -0,0 +1,12 @@
+Declarando um array -> my_array = ["valor1", "valor2","valor3"]
+
+Chamado um array por indice -> demo_array = [100, 200, 300, 400, 500]
+                               print demo_array[2] = 300 
+
+Array_Bidimensional (Matriz_Bidimensional)-> my_2d_array = [["casa", "amarela", "2000"],["casa","vermelha","1001"],["apt","branco&azul","2005"]]
+
+Hashes -> Coleção de pares com valores-chaves -> hash = {
+    key1 => value1,
+    key2 => value2,
+    key3 => value3
+  }
