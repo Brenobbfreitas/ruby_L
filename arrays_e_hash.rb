4555a9b8c83faa270d179e5cc279c8d825f0e2a7
Breno Barreto <41289855+Brenobbfreitas@users.noreply.github.com>
2021-02-23 18:45:48 -0300

    Add files via upload
    tipos de hash

--- arrays_e_hash.rb
+++ arrays_e_hash.rb
@@ -5,8 +5,15 @@ Chamado um array por indice -> demo_array = [100, 200, 300, 400, 500]
 
 Array_Bidimensional (Matriz_Bidimensional)-> my_2d_array = [["casa", "amarela", "2000"],["casa","vermelha","1001"],["apt","branco&azul","2005"]]
 
-Hashes -> Coleção de pares com valores-chaves -> hash = {
+Hashes -> Coleção de pares com valores-chaves; os valores são atribuidos usando "=>"; os valores são chamados atraves da chave -> 
+hash = {
     key1 => value1,
     key2 => value2,
     key3 => value3
   }
+  Outra forma de criar um hash é atraves -> Name_Hash = Hash.new -> O Hash Name_Hash é criando e está vazio.
+  Populando o hash ->
+    pets = Hash.new
+    pets["nero"] = "cat"
+  Acessando o Hash ->
+    print pets["nero"]
